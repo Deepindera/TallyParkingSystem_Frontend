48d635d0af9f04bb0a9da9b65e478b10ed8a74fe
Deepindera <Dee@DeeLaptop.CCHAIN.local>
2019-02-16 22:29:33 +1100

    Updated test

--- tallyparkingapp/src/components/ParkingFeeResult/ParkingFeeResult.test.js
+++ tallyparkingapp/src/components/ParkingFeeResult/ParkingFeeResult.test.js
@@ -46,6 +46,51 @@ describe('ParkingFeeResult renders parking properties properly', () => {
         const wrapper = shallow(<ParkingFeeResult calculated={true} amount={amount}/>)        
         expect(wrapper.contains(<tr className="feeRow"><td>Your Fee </td><td>$ {amount}</td></tr>)).toBeTruthy(); 
     });
+
+    it('should not  render placeholder text when calculated is false', () =>{        
+        const wrapper = shallow(<ParkingFeeResult calculated={true}/>)        
+        expect(wrapper.contains(<h4>Your Parking Fee will be shown here once calculated</h4>)).toBeFalsy(); 
+    });
    
 });
 
+describe('ParkingFeeResult does not render parking properties when calculated flag is false', () => {
+        
+    it('should not render rate properties', () =>{
+        var rateName ='Test Rate';
+        var rateType ='Test Rate Type';
+        const wrapper = shallow(<ParkingFeeResult calculated={false} rateName={rateName} rateType={rateType}/>)        
+        expect(wrapper.contains(<h3>{rateName} ({rateType})</h3>)).toBeFalsy();
+    });
+
+    it('should not render vehicle registration', () =>{
+        var vehicleRegistrationNo ='BMW 2322';       
+        const wrapper = shallow(<ParkingFeeResult calculated={false} vehicleRegistrationNo={vehicleRegistrationNo}/>)        
+        expect(wrapper.contains( <tr><td>Vehicle No</td><td>{vehicleRegistrationNo}</td></tr> )).toBeFalsy();
+    });
+
+    it('should not render entry time', () =>{
+        var entryTime ='21/2/2019 10:50';       
+        const wrapper = shallow(<ParkingFeeResult calculated={false} entryTime={entryTime}/>)        
+        expect(wrapper.contains(  <tr><td>Entry Time</td><td>{entryTime}</td></tr> )).toBeFalsy();
+    });
+
+    it('should not render exit time', () =>{
+        var exitTime ="21/2/2019 17:50";     
+        const wrapper = shallow(<ParkingFeeResult calculated={false} exitTime={exitTime} />)             
+        expect(wrapper.contains(<tr><td>Exit Time</td><td>{exitTime}</td></tr>)).toBeFalsy();
+    });
+
+    
+    it('should not render fee in special class', () =>{
+        var amount = 100;     
+        const wrapper = shallow(<ParkingFeeResult calculated={false} amount={amount}/>)        
+        expect(wrapper.contains(<tr className="feeRow"><td>Your Fee </td><td>$ {amount}</td></tr>)).toBeFalsy(); 
+    });
+
+    it('should  render placeholder text when calculated is false', () =>{        
+        const wrapper = shallow(<ParkingFeeResult calculated={false}/>)        
+        expect(wrapper.contains(<h4>Your Parking Fee will be shown here once calculated</h4>)).toBeTruthy(); 
+    });
+   
+});
